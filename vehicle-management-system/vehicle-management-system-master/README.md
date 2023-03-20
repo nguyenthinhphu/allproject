dc6be929be9d5cbc0bad0fd47ba2c5ac4e5304d1
le van duong <levanduong@luvina.net>
2023-03-20 14:59:02 +0700

    add

---
+++ vehicle-management-system/vehicle-management-system-master/README.md
@@ -0,0 +1,34 @@
+# Vehicle Management System
+   A web-based car rental management system that simplifies day-to-day car rental operations. The software stores and manages information on cars, clients, employees, invoices, and other entities. It ensures users authentication, security and manages users' profile. 
+
+## Features
+1. CRUD operations for Vehicle, Supplier, Employee, Client, Invoice and other entities.
+2. User authentication and security 
+3. User profile Management    
+
+## Technologies & Libraries 
+Project is created with:
+* Java 1.8
+* Spring Boot 2.4.1
+* Spring Boot Test
+* MySQL Server 8.0.22
+* Hibernate 
+* Tomcat
+* JPA
+* HTML, CSS
+* JavaScript, Bootstrap, JQuery, 
+* Thymeleaf
+	
+## Setup 
+To run this project,
+* Install [MySQL Server](https://dev.mysql.com/doc/mysql-installation-excerpt/8.0/en/windows-install-archive.html)
+* Install [JDK 1.8](https://www.oracle.com/ca-en/java/technologies/javase/javase-jdk8-downloads.html)
+
+## Running the application locally
+* Run your MySQL Server
+* Change username, password and datasource url from com.application.properties file according to your database connection.  
+* Run the com.saha.vehicleManagement.VehicleManagementApplication class from your IDE.
+
+## Acknowledgement: 
+[https://www.kindsonthegenius.com/spring-boot/build-a-complete-spring-boot-application-from-the-scratch-step-by-step/](https://www.kindsonthegenius.com/spring-boot/build-a-complete-spring-boot-application-from-the-scratch-step-by-step/)
+
---
+++ vehicle-management-system/vehicle-management-system-master/src/main/java/com/saha/vehicleManagement/models/User.java
@@ -0,0 +1,72 @@
+package com.saha.vehicleManagement.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class User {
+		
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+	private int id;
+	private String username;
+	private String password;
+	private String firstname;
+	private String lastname;
+	
+	
+	
+	public User() {
+		super();
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
